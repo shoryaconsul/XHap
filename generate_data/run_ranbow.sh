13d6de3776c2efb7b121903a273fa53b934d0c7e
Shorya Consul <shoryaconsul@utexas.edu>
2023-06-06 22:02:09 -0500

    script to run Ranbow

--- generate_data/run_ranbow.sh
+++ generate_data/run_ranbow.sh
@@ -2,8 +2,8 @@
 
 RANBOW="ranbow/ranbow.py"  # Path to Ranbow Python file
 PAR="ranbow_params"  # Path to Ranbow params file
-#SCAF="ranbow_scaffold.list"  # Path to Ranbow scaffolds file
-SCAF="ranbow/toy/scaffolds.list"
+SCAF="ranbow_scaffold.list"  # Path to Ranbow scaffolds file
+#SCAF="ranbow/toy/scaffolds.list"
 
 NUMPROC=1  # Number of processors
 
@@ -20,7 +20,16 @@ do
   esac
 done
 
+if [[ -d ./ranbow_tmp ]];then
+  echo "Removing temporary directory"
+  rm -r ./ranbow_tmp
+fi
+
+mkdir -p ./ranbow_tmp
+
+echo "$REFFILE"".fai"
 awk '{print $1}' "$REFFILE"".fai" > $SCAF
+
 
 # Writing required parameters into Ranbow parameter file
 echo "-ploidy $PLOIDY" > $PAR
@@ -31,8 +40,11 @@ echo "-vcfFile $VCFFILE" >> $PAR
 echo "-selectedScf $SCAF" >> $PAR
 echo "-outputFolderBase $OUTBASE" >> $PAR
 
+
+WLEN="${WINLEN:=5}"
+
 python2 $RANBOW hap -mode index -par $PAR -processorIndex 0
 echo "Indexing done ------------------------------------"
-#python2 $RANBOW hap -mode hap -par $PAR -processorIndex 0 -WinLen $WINLEN
-#echo "Finished running RANBOW --------------------------------"
-#python2 $RANBOW hap -mode collect -par $PAR
+python2 $RANBOW hap -mode hap -par $PAR -processorIndex 0 -WinLen $WINLEN
+echo "Finished running RANBOW --------------------------------"
+python2 $RANBOW hap -mode collect -par $PAR
