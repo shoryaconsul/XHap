b29b539f4a1c519e74614ca0a1372c615b05e53e
Shorya Consul <shoryaconsul@utexas.edu>
2023-06-06 22:15:20 -0500

    updted semi-experimental data scripts

--- generate_data/gendata_longread_semiexp.bash
+++ generate_data/gendata_longread_semiexp.bash
@@ -9,18 +9,27 @@ CAECSEQ="./CAECseq_haplo.py"  # Path to CAECSeq python file
 HAPCUT="./HapCUT2"  # Folder containing HapCUT2 and htslib source files
 EXTRACTHAIRS="./HapCUT2/build/extractHAIRS"  # Path to extractHAIRs executable from HapCUT2
 VERBOSE=false
-INDEL=100  # Length of insertion/deletion
 
-while getopts f:o:n:v FLAG
+INS=0
+DEL=0
+while getopts f:o:n:c:i:d:v FLAG
 do
   case "${FLAG}" in
     f) REF=${OPTARG};;  # Path to reference genome
     o) OUTHEAD=${OPTARG};;  # Base name of generated haplotype files
     n) HAPNUM=${OPTARG};;  # Number of haplotypes to generate
+    c) COV=${OPTARG};;  # Coverage per haplotype
+    i) INS=${OPTARG};;  # Length of insertion
+    d) DEL=${OPTARG};;  # Length of deletion
     v) VERBOSE=true;;
-    *) echo "Invalid command line option: -$FLAG" ;;
+    *) echo "$(basename $0): Invalid command line option: -$FLAG" ;;
   esac
 done
+
+if [ $INS -gt 0 ] && [ $DEL -gt 0 ]; then
+  echo "Either only insertion or only deletion can be added."
+  exit 1
+fi
 
 : << 'COMMENT'
 REF=$2  # Path to reference genome
@@ -46,7 +55,8 @@ fi
 if [[ $MODEL == "poisson" ]];then
   python2 $HAPGEN -f $REF -o $OUTHEAD --model $MODEL -s "[0.04,0.01,0.01]" -m "{'A':'GCT','G':'ACT','C':'TAG','T':'ACG'}" -p $HAPNUM
 elif [[ $MODEL == "lognormal" ]];then
-  python2 $HAPGEN -f $REF -o $OUTHEAD --model $MODEL -s "[3.03,0,0]" --sdlog "[1.293,0,0]" -m "{'A':'GCT','G':'ACT','C':'TAG','T':'ACG'}" -p $HAPNUM
+ python2 $HAPGEN -f $REF -o $OUTHEAD --model $MODEL -s "[6.07,0,0]" --sdlog "[1.293,0,0]" -m "{'A':'GCT','G':'ACT','C':'TAG','T':'ACG'}" -p $HAPNUM
+  # python2 $HAPGEN -f $REF -o $OUTHEAD --model $MODEL -s "[3.03,0,0]" --sdlog "[1.293,0,0]" -m "{'A':'GCT','G':'ACT','C':'TAG','T':'ACG'}" -p $HAPNUM
 else
   echo "Invalid model specified"
   exit 1
@@ -67,11 +77,15 @@ rm "$HAPFOLDER/"*".fai"  # Remove extraneous faidx files
 cat "$HAPFOLDER/"*".fa" > "$HAPFOLDER/combined.fa"  # Combined FASTA files
 
 # Add indels to one of the genomes
-python3 add_indel.py -f "$HAPFOLDER/combined.fa" -i $INDEL
+if [ $INS -gt 0 ]; then
+  python3 add_indel.py -f "$HAPFOLDER/combined.fa" -i $INS -t "$HAPFOLDER/indel.txt"
+elif [ $DEL -gt 0 ]; then
+  python3 add_indel.py -f "$HAPFOLDER/combined.fa" -d $DEL -t "$HAPFOLDER/indel.txt"
+fi
 
 # Generate long reads using PBSIM2
 # length_* paramters are related to read length
-$PBSIM --depth 13.3 --prefix "$HAPFOLDER/$OUTHEAD" --hmm_model pbsim2/data//P6C4.model "$HAPFOLDER/combined.fa"
+$PBSIM --depth $COV --prefix "$HAPFOLDER/$OUTHEAD" --hmm_model pbsim2/data//P6C4.model "$HAPFOLDER/combined.fa"
 for i in `ls "$HAPFOLDER/$OUTHEAD"_*.fastq`; do cat $i >> "$HAPFOLDER/$OUTHEAD.fq"; done
 # rm "$HAPFOLDER/$OUTHEAD"_*.fastq "$HAPFOLDER/$OUTHEAD"_*.ref "$HAPFOLDER/$OUTHEAD"_*.maf
 
@@ -84,13 +98,18 @@ samtools sort "$HAPFOLDER/$OUTHEAD.sam" -o "$HAPFOLDER/$OUTHEAD""_sorted.bam"
 echo "Generated reads"
 
 # Generate read-SNP matrix
-THRESH=0.2
+if [ $HAPNUM -gt 3 ]; then
+  THRESH=0.2
+else
+  THRESH=0.3
+fi
+
 HAPLEN=$(awk '{print length }' $REF | tail -1)  # Find length of reference genome
 $EXTRACT_MATRIX -f $REF -s "$HAPFOLDER/$OUTHEAD.sam" -t $THRESH -z "$HAPFOLDER/$OUTHEAD" -k $HAPNUM \
 -b 0 -e $HAPLEN -q 0 -l 100 -i 560 
 
 # CAECSeq
-python $CAECSEQ "$HAPFOLDER/$OUTHEAD" -k $HAPNUM -n 5
+# python $CAECSEQ "$HAPFOLDER/$OUTHEAD" -k $HAPNUM -n 5
 
 : << 'COMMENT'
 # Variant calling
